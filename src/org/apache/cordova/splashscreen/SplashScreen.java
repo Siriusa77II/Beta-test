39be2350480aa4a0c650716c4b1cab36cf98cd1c
Quentin Silva <im@SiriusHD.net>
2018-07-26 13:26:14 +0200

    Update 1.1 add lisibility & song request & fix statusbar

--- src/org/apache/cordova/splashscreen/SplashScreen.java
+++ src/org/apache/cordova/splashscreen/SplashScreen.java
@@ -53,7 +53,7 @@ public class SplashScreen extends CordovaPlugin {
     // Enable functionality only if running on 4.x.x.
     private static final boolean HAS_BUILT_IN_SPLASH_SCREEN = Integer.valueOf(CordovaWebView.CORDOVA_VERSION.split("\\.")[0]) < 4;
     private static final int DEFAULT_SPLASHSCREEN_DURATION = 3000;
-    private static final int DEFAULT_FADE_DURATION = 1000;
+    private static final int DEFAULT_FADE_DURATION = 500;
     private static Dialog splashDialog;
     private static ProgressDialog spinnerDialog;
     private static boolean firstShow = true;
